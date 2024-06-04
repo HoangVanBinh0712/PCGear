ed93bf47b9ed3cf4fd2910f05dce75d5002f715f
LAPTOP-GK940M8V\ASUS <thebest11447@gmail.com>
2024-06-04 22:34:52 +0700

    Add S3Service

---
+++ src/main/java/pc/gear/config/aws/AwsConfiguration.java
@@ -0,0 +1,28 @@
+package pc.gear.config.aws;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class AwsConfiguration {
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder().credentialsProvider(awsCredentialsProvider()).build();
+    }
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder().credentialsProvider(awsCredentialsProvider()).build();
+    }
+
+    private AwsCredentialsProvider awsCredentialsProvider() {
+        return DefaultCredentialsProvider.builder().build();
+    }
+
+}
